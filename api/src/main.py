b6bc93789aeecfbfb359f227179a027b6489024f
shriraikode <smraikode@gmail.com>
2025-07-23 01:17:04 +0530

    Update main.py

--- api/src/main.py
+++ api/src/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="CRM DEV API", version="1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
